00c28cdf6f62f49f2d21ec46c7c449ccc385bbc8
Andres Frei <jereporopat@gmail.com>
2024-05-14 10:23:57 -0300

    ya casi

---
+++ Practica Csharp/Ejercicio I01 - Sobre-sobrescribiendo esas advertencias/Ejercicio I01 - Sobre-sobrescribiendo esas advertencias/Program.cs
@@ -0,0 +1,24 @@
+﻿//Crear un proyecto de consola que contenga la clase Sobrescrito.
+//Sobrescribir el método ToString para que retorne "¡Este es mi método ToString sobrescrito!".
+//Sobrescribir el método Equals para que retorne true si son del mismo tipo (objetos de la misma clase),
+//false caso contrario.
+//Sobrescribir el método GetHashCode para que retorne el número 1142510181.
+//Agregar en el método Main el siguiente código:
+
+using Ejercicio_I01___Sobre_sobrescribiendo_esas_advertencias;
+
+Console.Title = "Ejercicio Sobre-Sobrescrito";
+Sobrescrito objetoSobrescrito = new Sobrescrito();
+
+Console.WriteLine(objetoSobrescrito.ToString());
+
+string objeto = "¡Este es mi método ToString sobrescrito!";
+
+Console.WriteLine("----------------------------------------------");
+Console.Write("Comparación Sobrecargas con String: ");
+Console.WriteLine(objetoSobrescrito.Equals(objeto));
+
+Console.WriteLine("----------------------------------------------");
+Console.WriteLine(objetoSobrescrito.GetHashCode());
+
+Console.ReadKey();
