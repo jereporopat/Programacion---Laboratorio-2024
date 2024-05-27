d0c1021b06a351ab1e53dc1fca8102c90cf81101
Andres Frei <jereporopat@gmail.com>
2024-05-27 16:41:08 -0300

    27/05

---
+++ Practica Csharp/Ejercicio I01 - Torneo/Ejercicio I01 - Torneo/Program.cs
@@ -0,0 +1,12 @@
+﻿using TorneoEntidades;
+
+Torneo<EquipoFutbol> torneoFtubol = new Torneo<EquipoFutbol>("torneito");
+
+EquipoFutbol e1 = new EquipoFutbol("CAI", DateTime.Now);
+EquipoFutbol e2 = new EquipoFutbol("CABJ", DateTime.Now);
+
+_= torneoFtubol + e1;
+_= torneoFtubol + e2;
+
+Console.WriteLine(torneoFtubol.Mostrar());
+Console.WriteLine(torneoFtubol.JugarPartido);
---
+++ Practica Csharp/Ejercicio I01 - Torneo/TorneoEntidades/Equipo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TorneoEntidades
+{
+    public abstract class Equipo
+    {
+        public string nombre;
+        private DateTime fechaCreacion;
+
+        public Equipo(string nombre, DateTime fechaCreacion)
+        {
+            this.nombre = nombre;
+            this.fechaCreacion = fechaCreacion;
+        }
+        public static bool operator ==(Equipo a, Equipo b)
+        {
+            return a.nombre == b.nombre && a.fechaCreacion == b.fechaCreacion;
+        }
+        public static bool operator !=(Equipo a, Equipo b) { return !(a == b); }
+
+        public string Ficha()
+        {
+            return $"{nombre} fundado el {fechaCreacion.ToString("dd/MM/yyyy")}";
+        }
+    }
+}
